fb9d90ab3afac1f6f468a7cf5cccace17ec6d7b4
nataliia-tyrsa <nataliiatyrsa@gmail.com>
2025-09-14 00:44:59 +0200

    feat: #2 added header

--- pt-web/src/components/Header/Header.tsx
+++ pt-web/src/components/Header/Header.tsx
@@ -3,8 +3,7 @@ import logo from "src/assets/brightside-health-logo.svg";
 import {PATHS} from "src/routes/routes";
 import styles from "src/components/Header/Header.module.scss";
 
-const CTA_TEXT = "Поддержка";
-
+const CTA_TEXT = "Пройти тест";
 const NAV_LINKS_LEFT = [
   {label: "О проекте", to: PATHS.ABOUT},
   {label: "Состояния и эмоции", to: PATHS.MENTAL_HEALTH.LIST},
@@ -25,7 +24,7 @@ export function Header() {
     >
       <nav
         className={styles.nav}
-        aria-label="Navigation"
+        aria-label="Основная навигация"
       >
         <ul className={styles.navList}>
           {NAV_LINKS_LEFT.map(item => (
@@ -46,7 +45,7 @@ export function Header() {
         <Link
           to={PATHS.HOME}
           className={styles.logo}
-          aria-label="Home"
+          aria-label="На главную"
         >
           <img
             src={logo}
