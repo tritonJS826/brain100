819f79b261aa0f26b86a6a7e1f2f0366a89cf336
nataliia-tyrsa <nataliiatyrsa@gmail.com>
2025-09-15 23:03:07 +0200

    fix: #3 changed styles and routes

--- pt-web/src/App.tsx
+++ pt-web/src/App.tsx
@@ -1,11 +1,18 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Header} from "src/components/Header/Header";
 import {HomePage} from "src/pages/Homepage/Homepage";
+import {PATHS} from "src/routes/routes";
 
 export function App() {
   return (
-    <div>
-      <HomePage />
+    <BrowserRouter>
       <Header />
-    </div>
+      <Routes>
+        <Route
+          path={PATHS.HOME}
+          element={<HomePage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
