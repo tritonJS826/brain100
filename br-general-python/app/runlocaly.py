8306311780125e6d9a86e5bcc913bf66b4e32068
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2025-09-23 13:47:55 +0200

    feat: #14 add layered structure for local run

---
+++ br-general-python/app/runlocaly.py
@@ -0,0 +1,15 @@
+import uvicorn
+from app.settings import settings
+
+from app.mainlocaly import app
+
+if __name__ == "__main__":
+    port = settings.server_port
+    print(f"Starting server on port {port} (reload={settings.flag_reload})...")
+
+    uvicorn.run(
+        "app.mainlocaly:app",
+        host="0.0.0.0",
+        port=port,
+        reload=settings.flag_reload,
+    )
