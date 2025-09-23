3bbc36028b4fcd9195344844607d3e18bc9c06c6
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2025-09-23 13:47:55 +0200

    feat: #14 add layered structure for local run

---
+++ br-general-python/app/mainlocaly.py
@@ -0,0 +1,18 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+from app.db import db
+from app.api import api_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    await db.connect()
+    yield
+    # shutdown
+    await db.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(api_router)
