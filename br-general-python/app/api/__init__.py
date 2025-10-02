82d671be2cfa18041d6e5a11121e7af6a1d3691c
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2025-10-02 12:14:16 +0200

    fix: #22 BR-22-br-general-python-fix-auth

--- br-general-python/app/api/__init__.py
+++ br-general-python/app/api/__init__.py
@@ -6,8 +6,8 @@ from app.settings import settings
 
 api_router = APIRouter()
 api_router.include_router(health.router, prefix="/br-general/health", tags=["health"])
+api_router.include_router(auth.router, prefix="/br-general/auth", tags=["auth"])
+api_router.include_router(users.router, prefix="/br-general/users", tags=["users"])
 # For production, disable email send and user create endpoints
 if settings.env_type != "prod":
     api_router.include_router(email.router, prefix="/br-general/email", tags=["email"])
-    api_router.include_router(users.router, prefix="/br-general/users", tags=["users"])
-    api_router.include_router(auth.router, prefix="/br-general/auth", tags=["auth"])
--- br-general-python/app/main.py
+++ br-general-python/app/main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI
 
 from app.db import db
 from app.api import api_router
@@ -17,13 +17,3 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(api_router)
-
-
-@app.middleware("http")
-async def attach_tokens(request: Request, call_next):
-    response: Response = await call_next(request)
-    if hasattr(request.state, "new_tokens"):
-        tokens = request.state.new_tokens
-        response.headers["x-new-access-token"] = tokens["access_token"]
-        response.headers["x-new-refresh-token"] = tokens["refresh_token"]
-    return response
